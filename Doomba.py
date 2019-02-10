188bdf22c672f5d16a75410691b16fee198ff1ed
Shawn-v <mcdonnes@my.erau.edu>
2019-02-10 16:58:53 +0000

    Added songs

--- Doomba.py
+++ Doomba.py
@@ -9,11 +9,19 @@ class Doomba(threading.Thread):
 		self.bot = create2api.Create2()
 		self.bot.start()
 		self.bot.safe()
+		self.songs = [	"C4L G4M E4M E4L D4M C4M C4M F4L E4M E4M D4M D4M C4L C4M G4M E4M E4M D4M D4M C4M C4M A3L G3L pauseM C4M C4M G4M E4M E4M D4M D4M C4M C4M F4L E4M E4M D4M D4M C4L C4M G4M E4M E4M D4M D4M C4M C4M D4L A3L",
+						"D#5L pauseS D#5M E5M pauseM B4M G#4S D#5L pauseS D#5M E5M pauseM B4M G#4S D#5L pauseS D#5M E5M pauseM B4M G#4S D#5L pauseS D#5M E5M pauseM B4M G#4S",
+						"G#3S A#3S C#4S A#3S F4L F4L D#4L pauseM G#3S A#3S C4S G#3S D#4L D#4L C#4L pauseM  G#3S A#3S C#4S A#3S C#4L D#4M C4L A#3S G#3L pauseS G#3M D#4L C#4L",
+						"G4L E4L C4L G3L A3M B3M C4M A3L C4M G3L pauseM D4L G4L E4L C4L D4L E4M D4L E4M D4L"]
 
-	def playSong(self):
-		song = "C4L G4M E4M E4L D4M C4M C4M F4L E4M E4M D4M D4M C4L C4M G4M E4M E4M D4M D4M C4M C4M A3L G3L \
-			pauseM C4M C4M G4M E4M E4M D4M D4M C4M C4M F4L E4M E4M D4M D4M C4L C4M G4M E4M E4M D4M D4M C4M C4M D4L A3L"
+	def playSong(self,number):
+		song = self.songs[number]
 		self.bot.playFakeMidi(song)
+		if number == 0 :
+			lyrics = "Somebody once told me the world is gonna roll me I ain't the sharpest tool in the shed    She was looking kind of dumb with her finger and her thumb In the shape of an 'L' on her forehead" 
+			for i in range(len(lyrics)-4):
+				self.bot.digit_led_ascii(lyrics[i:i+4])
+				time.sleep(0.0735)
 
 	def stop(self):
 		self.bot.drive_straight(0)
@@ -36,9 +44,9 @@ class Doomba(threading.Thread):
 				self.bot.turn_counter_clockwise(200)
 			elif data == "5":
 				self.bot.safe()
-			elif data == "6":
+			elif data[0] == "6":
 				self.stop()
-				self.playSong()
+				self.playSong(int(data[1:]))
 			else:
 				self.stop()
 			#self.bot.drive_straight(0)
